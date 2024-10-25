85db791503462926d99f6f01cba49bd31cfff89f
sohuichoi <sohui.choi@hodoolabs.com>
2024-10-25 14:09:41 +0900

    chore: 불필요 코드 정리

--- src/app/headless-ui/layout.tsx
+++ src/app/headless-ui/layout.tsx
@@ -1,5 +1,3 @@
-import ListCard from '@/components/common/list-card';
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return <div className='list-container'>{children}</div>;
 };
